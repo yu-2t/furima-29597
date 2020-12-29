0c398c1273de3d9b567e999fca1360dea045105e
yu-2t <yk.19861210@gmail.com>
2020-12-29 17:07:27 +0900

    商品情報編集機能の修正２

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configre_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!, only:[:new]
+  
   private
   
   def configre_permitted_parameters
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController 
   before_action :set_item, only:[:show, :edit, :update]
-
+  before_action :authenticate_user!, only:[:new, :edit]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -22,9 +22,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless user_signed_in?
-      redirect_to root_path
-    end
     if current_user.id != @item.user_id
       redirect_to root_path
     end
