82430f9dea7b45c114f6ea1866a71cbc256f9036
yu-2t <yk.19861210@gmail.com>
2021-01-12 12:56:41 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configre_permitted_parameters, if: :devise_controller?
-  
+  before_action :basic_auth
   private
-  
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+
   def configre_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
   end
